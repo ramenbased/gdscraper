eb593225bbc10ba07e5aa4ab2f4408bcfb23fe65
ramenbased <jens.p.bucher@googlemail.com>
2024-06-21 17:57:28 +0200

    usr reports start

--- go.mod
+++ go.mod
@@ -2,10 +2,13 @@ module suckmydick
 
 go 1.22.0
 
-require github.com/chromedp/chromedp v0.9.5
+require (
+	github.com/Davincible/chromedp-undetected v1.3.8
+	github.com/chromedp/chromedp v0.9.5
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+)
 
 require (
-	github.com/Davincible/chromedp-undetected v1.3.8 // indirect
 	github.com/Xuanwo/go-locale v1.1.0 // indirect
 	github.com/chromedp/cdproto v0.0.0-20240202021202-6d0b6a386732 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	cu "github.com/Davincible/chromedp-undetected"
 	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/kb"
+	"golang.org/x/net/html"
 )
 
 func Er(err error) {
@@ -32,18 +35,55 @@ func login(ctx context.Context, url string) {
 	}
 }
 
-func test(ctx context.Context) {
+func getUserReportsListHTML(ctx context.Context) string {
+	var rv string
 	if err := chromedp.Run(ctx,
-		chromedp.Navigate("https://growdiaries.com/diaries/54096-royal-queen-seeds-northern-light-grow-journal-by-w0oxtard/week/321888"),
-		chromedp.Sleep(10*time.Second),
+		chromedp.Navigate("https://growdiaries.com/seedbank/royal-queen-seeds/northern-light/diaries"),
+		chromedp.Sleep(3*time.Second),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			//how many times scroll to load
+			for i := 0; i <= 1; i++ {
+				chromedp.KeyEvent(kb.End).Do(ctx)
+				chromedp.Sleep(2 * time.Second).Do(ctx)
+			}
+			return nil
+		}),
+		chromedp.OuterHTML(".report_boxs.reports_grid", &rv),
 	); err != nil {
 		log.Fatal(err)
 	}
+	return rv
+}
+
+func compileUserReportsList(outer string) {
+	sr := strings.NewReader(outer)
+	doc, err := html.Parse(sr)
+	Er(err)
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			for _, i := range n.Attr {
+				//fmt.Printf("KEY: %v, VAL: %v", n.Key, n.Val)
+				if i.Key == "href" {
+					for _, y := range n.Attr {
+						if y.Key == "class" && y.Val == "name" {
+							fmt.Println(i.Val)
+						}
+					}
+
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
 }
 
 func main() {
-	var url = "https://growdiaries.com/auth/signin"
-	var outer string
+	//var url = "https://growdiaries.com/auth/signin"
 
 	ctx, cancel, err := cu.New(cu.NewConfig(
 		//cu.WithHeadless(),
@@ -57,25 +97,7 @@ func main() {
 
 	//login
 
-	login(ctx, url)
-	test(ctx)
-
-	fmt.Println(outer)
-
-	/*
-		sr := strings.NewReader(outer)
-		doc, err := html.Parse(sr)
-		Er(err)
-
-		var f func(*html.Node)
-		f = func(n *html.Node) {
-			if n.Type == html.TextNode {
-				fmt.Println(n.Data)
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c)
-			}
-		}
-		f(doc)
-	*/
+	//login(ctx, url)
+	userReportsList := getUserReportsListHTML(ctx)
+	compileUserReportsList(userReportsList)
 }
