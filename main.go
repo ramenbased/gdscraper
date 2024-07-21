173c702377b0287c6fdde7cfbc3fe767c506a64a
ramenbased <jens.p.bucher@googlemail.com>
2024-07-21 09:29:55 +0200

    test

--- main.go
+++ main.go
@@ -204,7 +204,7 @@ func getUserDiary(ctx context.Context, URLs []string, tbl *data.Tables) {
 }
 
 func main() {
-	f, err := os.OpenFile("logfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("logfile.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
